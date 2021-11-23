8842b96453a9d3942209c40039b0f10ce74cae9a
BEN-MTP <minhmanh.office.uet@gmail.com>
2021-11-23 17:09:23 +0700

    add | update project spring example

---
+++ spring-boot-10-RequestMapping-PostMapping-ModelAttribute-RequestParam-WebTodo-Thymeleaf/src/main/java/com/springboot/springboot10webtodothymeleaf/SpringBoot10WebtodoThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.springboot10webtodothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10WebtodoThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot10WebtodoThymeleafApplication.class, args);
+        System.out.println("Spring Boot 10 RequestMapping - PostMapping-ModelAttribute-RequestParam-WebTodo-Thymeleaf");
+    }
+
+}
---
+++ spring-boot-10-RequestMapping-PostMapping-ModelAttribute-RequestParam-WebTodo-Thymeleaf/src/test/java/com/springboot/springboot10webtodothymeleaf/SpringBoot10WebtodoThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot10webtodothymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot10WebtodoThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
