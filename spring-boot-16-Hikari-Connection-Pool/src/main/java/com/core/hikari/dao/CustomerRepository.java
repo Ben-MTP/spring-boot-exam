04bc0de5b3be18bccbfb3d26844bf5a53a9b62a9
BEN-MTP <minhmanh.office.uet@gmail.com>
2022-04-23 17:19:09 +0700

    update project

---
+++ spring-boot-16-Hikari-Connection-Pool/src/main/java/com/core/hikari/dao/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.core.hikari.dao;/**
+ * @project spring-boot-16-Hikari-Connection-Pool
+ * @author ManhKM on 4/19/2022
+*/
+    public class CustomerRepository {
+}
---
+++ spring-boot-16-Hikari-Connection-Pool/src/main/java/com/core/hikari/model/Customer.java
@@ -0,0 +1,6 @@
+package com.core.hikari.model;/**
+ * @project spring-boot-16-Hikari-Connection-Pool
+ * @author ManhKM on 4/19/2022
+*/
+    public class Customer {
+}
